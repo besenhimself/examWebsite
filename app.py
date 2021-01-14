f070e407f695507c42e4fdabbe5d77e066294f7f
Kris Petrov <petrov.kris03@gmail.com>
2021-01-14 22:16:02 +0200

    First Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['user']
+        return redirect(url_for('success', name = user))
+    else:
+        return render_template('login.html')
+
+@app.route('/home/<name>')
+def success(name):
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
