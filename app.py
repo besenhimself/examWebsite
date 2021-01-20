dd5de0c3a42e133ce68727e26c3abc4481097ae0
Kris Petrov <petrov.kris03@gmail.com>
2021-01-20 21:07:30 +0200

    Login Form

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, url_for, redirect, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import Length, InputRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+class Login(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min = 3, max = 16)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min = 4, max = 24)])
 
 @app.route('/')
 def index():
@@ -8,12 +16,9 @@ def index():
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        user = request.form['user']
-        return redirect(url_for('success', name = user))
-    else:
-        return render_template('login.html')
+    form = Login()
 
+    return render_template('login.html', form = form)
 @app.route('/home/<name>')
 def success(name):
     return render_template('home.html')
